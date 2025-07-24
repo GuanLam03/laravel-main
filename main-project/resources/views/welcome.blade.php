0bca9618390bb41350f0c081c8762f8527facbd9
Guan lam <hgledgetech@gmail.com>
2025-07-24 15:11:03 +0800

    edit sub

--- main-project/resources/views/welcome.blade.php
+++ main-project/resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
        <ul>
         <li>Main</li>
-        <li>Sub</li>
+        <a href="/sub">Sub</a>
        </ul>
     </body>
 </html>
--- main-project/routes/web.php
+++ main-project/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
+
+View::addNamespace('submodule', base_path('submodule-repo-new/resources/views'));
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/sub', function () {
+    return view('submodule::sub');
 });
